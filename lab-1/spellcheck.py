d6403723222619769b3fcd6266ac97cc3653b8de
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-09-25 17:28:41 -0400

    Spell checking and suggestions done

--- lab-1/spellcheck.py
+++ lab-1/spellcheck.py
@@ -1,26 +1,51 @@
-import argparse, nltk, string
+import re
+import argparse
+import string
 
-parser = argparse.ArgumentParser(description='spell checker')
 
-parser.add_argument('-f',
-                    type=str,
-                    metavar='',
-                    help = 'file to spell check',
-                    required = True)
-args = parser.parse_args()
+def insert_char(word, index, c):
+    if index == 0:
+        return(c + word)
+    elif index + 1 == len(word):
+        return(word + c)
+    else:
+        return(word[:index] + c + word[index+1:])
 
-with open('dictionary.txt', 'r') as f:
-    dictionary = f.read().splitlines()
 
-with open(args.f, 'r') as f:
-    text = f.read().splitlines()
-    for line in text:
-        line = line.lower()
-        #line = line.translate(str.maketrans(string.punctuation))
-        #line = line.translate(str.maketrans('0123456789'))
-        line = line.split(" ")
-        for word in line:
-            word = word.translate(str.maketrans('', '', string.punctuation))
-            word = word.translate(str.maketrans('','', '0123456789’‘“”'))
-            if word not in dictionary:
-                print(word)
+if __name__ == '__main__':
+    # ArgumentParser
+    parser = argparse.ArgumentParser(description='spell checker')
+    parser.add_argument('-f',
+                        type=str,
+                        metavar='',
+                        help='file to spell check',
+                        required=True)
+    args = parser.parse_args()
+
+    with open('dictionary.txt', 'r') as f:
+        dictionary = f.read().splitlines()
+
+    # Spell checking
+    mispelled = list()
+    with open(args.f, 'r') as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                if re.findall(r"[0-9]", word):
+                    continue
+                word = word.lower()
+                word = word.translate(str.maketrans('', '',
+                                                    string.punctuation))
+                if word not in dictionary:
+                    mispelled.append(word)
+
+    # Spell suggestions
+    suggestions = list()
+    for word in mispelled:
+        for index, char in enumerate(word):
+            for c in string.ascii_lowercase:
+                suggestion = insert_char(word, index, c)
+                if suggestion in dictionary:
+                    suggestions.append(suggestion)
+
+    print(suggestions)
